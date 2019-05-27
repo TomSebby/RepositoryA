742ceb5b70bf92befd027fb8bb6c9d1b3f07701e
Dell <Dell@DESKTOP-BGE7FLA>
2019-05-27 21:51:09 +0530

    push the code 1

---
+++ src/test/java/Test/DemoA2.java
@@ -0,0 +1,16 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA2 
+{
+     @Test
+     public void testA()
+     {
+    	 
+    	 Reporter.log("demo maven",true);
+     }
+	
+	
+}
